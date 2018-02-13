2f8a791a2e5acac3ae195174c7d2a0027f96bf35
NicolasMCP <nicolas.mcp@gmail.com>
2018-02-12 23:01:35 -0200

    dividindo os arquivos em arquivos menores, por temas (PHPstorm)

--- usuarios.php
+++ usuarios.php
@@ -13,7 +13,8 @@ include("cabecalho.php");
     <section>
     <!--linux-->
 
-        <h1>Dicas do Linux Debian Stretch 9.3</h1>
+        <h1>Dicas do Linux Debian <span style="font-family: 'Noto Sans', sans-serif;">9.3</span> Stretch e</h1>
+        <h1>Debian <span style="font-family: 'Noto Sans', sans-serif;">10</span> Buster</h1>
 
         <h2>Usuários e Grupos</h2>
 

40439fed41beacd12a1d8495cd802a80fdb402f0
NicolasMCP <nicolas.mcp@gmail.com>
2018-05-19 23:15:43 -0300

    Ajustes nos cabeçalhos...

--- google.php
+++ google.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <!--
-Autor  : Nicolas Ramos
-Data   : 20/01/2018
-Projeto: Dicas
-Meta   : Organizar as dicas do Google (agora neste html, antes em OOo)
+    Autor: Nicolas Ramos
+    Data : 20/01/2018
+    Nota : Organizar as dicas do Google (agora neste html, antes em OOo)
 -->
 
 <?php

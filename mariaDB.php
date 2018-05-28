b1ca708c5a28625cb78d8b0e7440558f619c2507
NicolasMCP <nicolas.mcp@gmail.com>
2018-05-27 21:38:16 -0300

    Mudança de debian 9.3 a debian 9.4, inclussão de novos links

--- mariaDB.php
+++ mariaDB.php
@@ -12,38 +12,64 @@ include("cabecalho.php");
 <section>
 
     <h1>MariaDB no Linux</h1>
-    <h2 class="reduzido">Testado em Linux Debian <span style="font-family: 'Noto Sans', sans-serif;">9.3</span> Stretch e no Debian <span style="font-family: 'Noto Sans', sans-serif;">10</span> Buster</h2>
-    <p class="comum">Nota: A instala&ccedil;&atilde;o para Linux Mint esta logo abaixo.</p>
+    <h2 class="reduzido">Testado em Linux Debian <span style="font-family: 'Noto Sans', sans-serif;">9.4</span> Stretch e no Debian <span style="font-family: 'Noto Sans', sans-serif;">10</span> Buster</h2>
+    <a href="#linux-mint" class="a" target="_parent">Instala&ccedil;&atilde;o no Linux Mint.</a>
     <!--MariaDB-->
     <h3>Instalando MariaDB no Debian</h3>
     <p class="comum">S&oacute; precisa descomentar os reposit&oacute;rios oficiais principais</p>
+    <p class="comum">Verifique se tem estes reposit&oacute;rios</p>
     <p class="miniatura">C&oacute;digo</p>
     <div class="box sombra">
-        $ sudo apt install mysql-server<br/>
+        $ sudo cat /etc/apt/sources.list<br/>
+        <br>
+        <p style="font-size: 12px">
+            # verificar se temos estes reposit&oacute;rios, em especial o non-free (no final)<br>
+            <br>
+            ## Debian - stretch<br>
+            deb http://ftp.us.debian.org/debian/ stretch main contrib non-free<br>
+            deb-src http://ftp.us.debian.org/debian/ stretch main contrib non-free<br>
+            ## Atualiza&ccedil;&otilde;es de seguran&ccedil;a<br>
+            deb http://security.debian.org/ stretch/updates main contrib non-free<br>
+            deb-src http://security.debian.org/ stretch/updates main contrib non-free<br>
+        </p>
     </div>
 
-    <p class="comum">Se n&atilde;o funcionar verifique se os seus Repositorios tem no final 'non-free'</p>
-    <p class="miniatura">Exemplo</p>
+
+    <p class="miniatura">C&oacute;digo</p>
     <div class="box sombra">
-        # Oficial de stretch<br/>
-         deb http://ftp.us.debian.org/debian/ stretch main contrib non-free<br/>
-        <br/>
-        # Atualiza&ccedil;&odblac;es de seguridade<br/>
-         deb http://security.debian.org/debian-security stretch/updates main contrib non-free<br/>
+        $ sudo apt install mariadb-server<br/>
     </div>
 
-    <p class="comum">Nota: Tomara que mudem isso logo, porque gera bastante confus&atilde;o, pensamos que estamos instalando o MySQL...</p>
+    <p class="comum">Vamos verificar a vers&atilde;o instalada</p>
+    <p class="miniatura">C&oacute;digo</p>
+    <div class="box sombra">
+        $ mysql --version
+        <br><br>
+        <p style="font-size: 12px">
+            mysql  Ver 15.1 Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64) using readline 5.2
+        </p>
+    </div>
 
     <?php
     include("pre-rodape.php");
     ?>
-    <h5>Instalando MariaDB no Linux Mint</h5>
+    <h5 id="linux-mint">Instalando MariaDB no Linux Mint</h5>
     <h4 class="reduzido">Testado no Linux Mint <span style="font-family: 'Noto Sans', sans-serif;">18.3</span></h4>
 
     <p class="comum">No Linux Mint &eacute; mais molezinha ainda, basta digitar:</p>
     <p class="miniatura">C&oacute;digo</p>
     <div class="box sombra">
         sudo apt install mariadb-server
+    </div>
+
+    <p class="comum">Vamos verificar a vers&atilde;o instalada</p>
+    <p class="miniatura">C&oacute;digo</p>
+    <div class="box sombra">
+        $ mysql --version
+        <br><br>
+        <p style="font-size: 12px">
+            mysql  Ver 15.1 Distrib 10.0.34-MariaDB, for debian-linux-gnu (x86_64) using readline 5.2            
+        </p>
     </div>
 
 </section>

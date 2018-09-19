ee1eeb7783d51bb1c1ca79414fb3f88cc2157dda
NicolasMCP <nicolas.mcp@gmail.com>
2018-09-19 11:32:32 -0300

    Alterações de icones, inclir repositórios

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # infodata.xyz
-AREA DE DESENVOLVIMENTO do Site infodata.xyz
+AREA DE DESENVOLVIMENTO do Site [infodata.xyz](https://infodata.xyz/src/index.php)
 
-GNU GENERAL PUBLIC LICENSE V.2
+
+![CRAN](https://img.shields.io/cran/l/devtools.svg?style=for-the-badge)
+
+
+> Site Infodata, Licença: GNU GENERAL PUBLIC LICENSE V.2
+
+> Tratamento de erros, Licença: GNU GPL 3
+
+> Icones, diversas Licenças (veja no proprio site)
--- src/cabecalho.php
+++ src/cabecalho.php
@@ -33,7 +33,7 @@ echo '<html lang="pt-br">
 <div class="fix">
     <header>
         <input type="checkbox" id="chk_menu">
-        <label for="chk_menu" id="lbl_menu"><span class="icon-reorder"></span></label>
+        <label for="chk_menu" id="lbl_menu"><span class="icon-menu"></span></label>
         <nav class="menu">
             <ul>
                 <li><a href="index.php"><span class="icon-home3"></span>Home</a></li>
@@ -43,10 +43,10 @@ echo '<html lang="pt-br">
         
                     <label for="chk_browser" id="lbl_browser"><span class="icon-earth"></span>Navegadores<i class="icon-select-arrows"></i></label>
                     <ul>
-                        <li><a href="firefox.php"><span class="icon-firefox2"></span>FireFox</a></li>
-                        <li><a href="chrome.php"><span class="icon-chrome2"></span>Chrome</a></li>
+                        <li><a href="firefox.php"><span class="icon-firefox"></span>FireFox</a></li>
+                        <li><a href="chrome.php"><span class="icon-chrome"></span>Chrome</a></li>
                         <li><a href="opera.php"><span class="icon-opera"></span>Opera</a></li>
-                        <li><a href="ie.php"><span class="icon-internet-explorer"></span>Internet Explorer</a></li>
+                        <li><a href="ie.php"><span class="icon-IE"></span>Internet Explorer</a></li>
                     </ul>
                 </li>
                 <li>
@@ -59,28 +59,29 @@ echo '<html lang="pt-br">
                         <li><a href="idioma.php"><span class="icon-language"></span>Idioma</a></li>
                         <li><a href="samba.php"><span class="icon-folders"></span>Samba</a></li>
                         <li><a href="usuarios.php"><span class="icon-user"></span>Usu&aacute;rios</a></li>
+                        <li><a href="conky.php"><img src="../img/conky17.png">Conky</a></li>
                     </ul>
                </li>
                 <li>
                     <input type="checkbox" id="chk_java">
         
-                    <label for="chk_java" id="lbl_java"><span class="icon-local_cafe"></span>Java<i class="icon-select-arrows"></i></label>
+                    <label for="chk_java" id="lbl_java"><img src="../img/java.svg" height="20px" width="30px">Java<i class="icon-select-arrows"></i></label>
                     <ul>
                         <li><a href="jdk.php"><span class="icon-mug"></span>Java JDK</a></li>
-                        <li><a href="java-painel.php"><span class="icon-document-file-java2"></span>Painel Java</a></li>
+                        <li><a href="java-painel.php"><img src="../img/java.svg" height="20px" width="30px">Painel Java</a></li>
                     </ul>
                 <li>
                     <input type="checkbox" id="chk_lamp">
         
-                    <label for="chk_lamp" id="lbl_lamp"><span class="icon-list-ul"></span>LAMP e Cia.<i class="icon-select-arrows"></i></label>
+                    <label for="chk_lamp" id="lbl_lamp"><span class="icon-list"></span>LAMP e Cia.<i class="icon-select-arrows"></i></label>
                     <ul>
                         <li><a href="apache.php"><span class="icon-feather"></span>Apache</a></li>
                         <li><a href="mysql.php"><img src="../img/mysql.png">MySQL</a></li>
                         <li><a href="mariaDB.php"><img src="../img/mariadb.png" height="20px" width="28px">MariaDB</a></li>
                         <li><a href="firebird.php"><img src="../img/firebird-logo-32pb.png" height="20px" width="28px">Firebird</a></li>
                         <li><a href="firebird.php#FlameRobin"><img src="../img/flamerobin-logo24pb.png" height="20px" width="28px">FlameRobin</a></li>
-                        <li><a href="php.php"><span class="icon-file-php"></span>PHP Debian</a></li>
-                        <li><a href="php.php#linux-mint"><span class="icon-file-php"></span>PHP Mint</a></li>
+                        <li><a href="php.php"><img src="../img/php.svg" height="22px" width="34px">PHP Debian</a></li>
+                        <li><a href="php.php#linux-mint"><img src="../img/php.svg" height="22px" width="34px">PHP Mint</a></li>
                         <li><a href="node.php"><img src="../img/nodejs.png" height="20px" width="28px">Node.js</a></li>
                     </ul>
                 <li>
@@ -97,14 +98,27 @@ echo '<html lang="pt-br">
                 <li>
                     <input type="checkbox" id="chk_ide">
         
-                <label for="chk_ide" id="lbl_ide"><span class="icon-lightbulb"></span>Ide,SO,App<i class="icon-select-arrows"></i></label>
+                    <label for="chk_ide" id="lbl_ide"><span class="icon-lightbulb"></span>Ide,SO,App<i class="icon-select-arrows"></i></label>
                     <ul>
-                        <li><a href="phpstorm.php"><span class="icon-bulb"></span>PHPStorm</a></li>
+                        <li><a href="phpstorm.php"><span class="icon-lightbulb"></span>PHPStorm</a></li>
                         <li><a href="netbeans.php"><span class="icon-codepen"></span>NetBeans</a></li>
                         <li><a href="android.php"><span class="icon-android"></span>Android</a></li>
                         <li><a href="vim.php"><img src="../img/vim.png" height="20px" width="28px">Vim</a></li>
                     </ul>
                 <li><a href="icomoon.php"><span class="icon-IcoMoon"></span>IcoMoon</a></li>
+                
+                <li>
+                    <input type="checkbox" id="chk_github">
+        
+                <label for="chk_github" id="lbl_github"><span class="icon-github"></span>NotABug<i class="icon-select-arrows"></i></label>
+                <ul>
+                        <li><a href="https://notabug.org/NicolasMCP/Python" target="_blank"><span class="icon-git"></span>Python</a></li>
+                        <li><a href="https://notabug.org/NicolasMCP/Java" target="_blank"><span class="icon-git"></span>Java</a></li>
+                        <li><a href="https://notabug.org/NicolasMCP/ExportCatalog" target="_blank"><span class="icon-git"></span>C# ExportCatalog</a></li>
+                        <li><a href="https://notabug.org/NicolasMCP/SisReenvioGeral" target="_blank"><span class="icon-git"></span>VB e-mail</a></li>
+                        <li><a href="https://notabug.org/NicolasMCP/totop" target="_blank"><span class="icon-git"></span>jQuery ToTop</a></li>
+                </ul>
+                </li>
                 <li><a href="licencas.php"><span class="icon-copyright"></span>Licen&ccedil;as</a></li>
             </ul>
         </nav>

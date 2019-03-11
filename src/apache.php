9f3f3ff7316ce2401b973ba38c8094ada27b5e08
NicolasMCP <nicolas.mcp@gmail.com>
2019-03-11 00:20:43 -0300

    incluir MX-Linux18.1 e up outros SO

--- src/apache.php
+++ src/apache.php
@@ -13,9 +13,10 @@ include("cabecalho.php");
 
     <h1>Instala&ccedil;&atilde;o do Apache no Linux</h1>
 
-    <h2 class="reduzido">Testado no Debian <span style="font-family: 'Noto Sans', sans-serif;">9.4</span> Stretch,
-        no Debian <span style="font-family: 'Noto Sans', sans-serif;">10</span> Buster e no<br/>
-        Linux Mint <span style="font-family: 'Noto Sans', sans-serif;">19</span> Tara</h2>
+    <h2 class="reduzido">Testado no Debian <span style="font-family: 'Noto Sans', sans-serif;">9.8</span> Stretch,
+        no Debian <span style="font-family: 'Noto Sans', sans-serif;">10</span> Buster,<br/>no
+        Linux Mint <span style="font-family: 'Noto Sans', sans-serif;">19.1</span> Tessa e no
+        MX-Linux <span style="font-family: 'Noto Sans', sans-serif;">18.1</span></h2>
 
     <h3>Instalando o Apache</h3>
     <p class="comum">A instala&ccedil;&atilde;o &eacute; extremamente simples, basta digitar:</p>
@@ -38,9 +39,16 @@ include("cabecalho.php");
     <div class="box sombra">
         <a href="http://localhost/" class="a" target="_blank">http://localhost/</a>
     </div>
-    <p class="comum">Nota: No Linux Debian 9 Stretch ser&aacute; instalado <strong>Apache &eacute; 2.4.25</strong></p>
+    <p class="comum"><strong>Nota:</strong> No Linux Debian 9.8 Stretch ser&aacute; instalado <strong>Apache &eacute; 2.4.25</strong></p>
     <p class="comum">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No Linux Debian 10 Buster ser&aacute; instalado <strong>Apache 2.4.29</strong></p>
-    <p class="comum">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No Linux Mint a vers&atilde;o do <strong>Apache &eacute; 2.4.29</strong></p>
+    <p class="comum">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No Linux Mint 19.1 a vers&atilde;o do <strong>Apache &eacute; 2.4.29</strong></p>
+    <p class="comum">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No MX-Linux 18.1 a vers&atilde;o do <strong>Apache &eacute; 2.4.25</strong>
+     e embora nele tenha a op&ccedil;&atilde;o de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;instalar automaticamente o Apache, MariaDB e PHP
+        7.0, se voc&ecirc; optar por essa &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;abordagem (instalar tudo em alguns clicks), <strong>n&atilde;o fa&ccedil;a esta instala&ccedil;&atilde;o
+        manual.</strong><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Em vez disso va em: Configura&ccedil;&otilde;es &gt; MX Ferramentas &gt; MX-Instalador de Pacotes 
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt; Servidores.
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Selecione 'Local Web Server - apache2, php7, mariaDB' e clique no bot&atilde;o<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'Instalar'<br/>
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pessoalmente prefiro a instalação manual, não somente por ter mais controle,<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;mas porque precisso usar versões mais atuais do PHP.</p>
 
 </section>
 

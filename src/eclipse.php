122b9bea08ceaafc5cb123300ef134e94da7ab84
NicolasMCP <nicolas.mcp@gmail.com>
2019-12-09 12:09:34 -0300

    eclipse 3 jdks

---
+++ src/eclipse.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+    Autor: Nicolas Ramos
+    Nota : Organizar as dicas do chrome (agora neste html, antes em OOo)
+-->
+
+<?php
+include("cabecalho.php");
+?>
+
+<section>
+
+    <!--chrome-->
+
+    <h1>Eclipse</h1>
+
+    <h2>Instalar, Configurar com os 3 openJDK e dicionario em Portugu&ecirc;s.</h2>
+    <p class="comum">V&iacute;deo de como fazer...</p>
+    <br>
+    <iframe width="800" height="450" src="https://www.youtube.com/embed/6gniT7jDLf0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+</section>
+
+<?php
+for($i=0; $i<17; $i++) {echo'<br>';}
+include("rodape.php");
+?>

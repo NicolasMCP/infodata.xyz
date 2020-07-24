a08c41f819e5210560d4a2aa695497211e312311
NicolasMCP <nicolas.mcp@gmail.com>
2020-07-24 15:35:51 -0300

    incluindo PHP 7.4.8

--- src/php.php
+++ src/php.php
@@ -12,9 +12,100 @@ include("cabecalho.php");
 <!--php-->
     <h1>Instalando o PHP em Linux</h1>
 <!----------------------------------------------------------------------------------------------------------------->
-    <h3>Instalar PHP <span style="font-family: 'Noto Sans', sans-serif;">7.3.11</span> no Linux Mint</h3>
-    <h4 class="reduzido">Testado em Linux Mint 19.3, 19.2 e 18.3</h4>
+    <h3>Instalar PHP <span style="font-family: 'Noto Sans', sans-serif;">7.4</span> no Linux Mint</h3>
+    <h4 class="reduzido">Testado em Linux Mint 20 Ulyana.</h4>
     <a href="#linux-debian" class="a" target="_parent">Instala&ccedil;&atilde;o no Linux Debian.</a>
+
+    <p class="comum">Instalando o PHP.</p><br>
+    <a href="apache.php" class="a" target="_blank">Pre-requisito, instala&ccedil;&atilde;o do Servidor Apache</a><br>
+    <a href="mariaDB.php" class="a" target="_blank">Opcional, instala&ccedil;&atilde;o do MariaDB</a><br>
+    <br>
+    <p class="comum"><b>Nota:</b> a vers&atilde;o da instala&ccedil;&atilde;o do v&iacute;deo &eacute; 7.3.11 e os comandos logo abaixo s&atilde;o para a vers&atilde;o
+        7.4</p><br>
+    <a href="#PHP7.3.11" class="a" target="_parent">Instala&ccedil;&atilde;o no Linux Mint do PHP 7.3.11</a><br><br>
+    <iframe width="800" height="450" src="https://www.youtube.com/embed/YS7O316Ycxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <br>
+    <br>
+    <p class="comum">Instalando o PHP no Linux Mint</p>
+    <p class="miniatura">C&oacute;digo</p>
+    <div class="box sombra">
+        <code>
+            $ sudo apt install software-properties-common<br>
+            $ sudo add-apt-repository ppa:ondrej/php<br>
+        </code>
+        <p class="miniatura">Atualizar pelo 'gerenciador de atualiza&ccedil;&odblac;es'<br>
+            Atualizar &gt; Instalar Atualiza&ccedil;&odblac;es<br><br></p>
+    </div>
+
+
+    <p class="comum">A instala&ccedil;&atilde;o dos pacotes...</p>
+    <p class="miniatura">C&oacute;digo</p>
+    <div class="box sombra">
+        <code>
+            $ sudo apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php7.4-curl php7.4-json php7.4-xsl php7.4-cli
+            php7.4-mysql php7.4-dev php7.4-sqlite3 php7.4-mbstring php7.4-curl php-memcached
+            php-pear php7.4-opcache php7.4-readline php7.4-gd php7.4-zip php7.4-xml php7.4-cgi<br>
+            $ sudo apt install dh-php<br>
+            $ sudo apt-get install php-cgi<br>
+            $ ls -la /usr/bin/php7.4<br>
+            $ sudo update-alternatives --set php /usr/bin/php7.4<br>
+            $ sudo service apache2 restart<br>
+        </code>
+    </div>
+
+
+    <p class="comum">Teste a instala&ccedil;&atilde;o</p>
+    <p class="miniatura">C&oacute;digo</p>
+    <div class="box sombra">
+        <code>
+            $ php -v<br>
+        </code>
+
+        <p style="font-size: 12px;"><br>
+            PHP 7.4.8 (cli) (built: Jul 13 2020 16:46:22) ( NTS )<br>
+            Copyright (c) The PHP Group<br>
+            Zend Engine v3.4.0, Copyright (c) Zend Technologies<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;with Zend OPcache v7.4.8, Copyright (c), by Zend Technologies<br>
+        </p>
+    </div>
+
+
+    <p class="comum">Agora crie um arquivo php de informa&ccedil;&odblac;es</p>
+    <p class="miniatura">C&oacute;digo</p>
+    <div class="box sombra">
+        <code>
+            $ cd /var/www/html<br>
+            $ sudo vim phpinfo.php<br>
+        </code>
+    </div>
+
+    <p class="miniatura">Incluir no arquivo</p>
+    <div class="box sombra">
+        <code>
+            <span style="color: orange">&lt;?</span><span style="color: red">php</span><br>
+            <span style="color: darkviolet">phpinfo</span><span style="color: darkred">();</span><br>
+            <span style="color: orange">?&gt;</span><br>
+        </code>
+    </div>
+
+    <p class="comum">Abra no seu Navegador</p>
+    <p class="miniatura">Web</p>
+    <div class="box sombra">
+        <a href="http://localhost/phpinfo.php" class="a" target="_blank">http://localhost/phpinfo.php</a>
+    </div>
+    <br>
+    <a href="#caso-de-erro" class="a" target="_parent">Caso de erro na instalação do PHP.</a>
+
+    <?php
+    include("pre-rodape.php");
+    ?>
+
+    <!----------------------------------------------------------------------------------------------------------------->
+
+    <h3 id="PHP7.3.11">Instalar PHP <span style="font-family: 'Noto Sans', sans-serif;">7.3.11</span> no Linux Mint</h3>
+    <h4 class="reduzido">Testado em Linux Mint 19.3 Tricia, 19.2 Tina e 18.3 Sylvia</h4>
+    <a href="#linux-debian" class="a" target="_parent">Instala&ccedil;&atilde;o no Linux Debian.</a>
+    <a href="#top" class="a" target="_parent">Instala&ccedil;&atilde;o no Linux Mint do PHP 7.4</a>
 
     <p class="comum">Instalando o PHP. Visto que muitos estão usando a nova versão do wordpress, e eu mesmo tambem
         quero esperimenta-la, estou mudando a versão do PHP para 7.3, que é compatível com o mesmo.</p>
@@ -92,6 +183,16 @@ include("cabecalho.php");
     <div class="box sombra">
         <a href="http://localhost/phpinfo.php" class="a" target="_blank">http://localhost/phpinfo.php</a>
     </div>
+    <br>
+    <a href="#caso-de-erro" class="a" target="_parent">Caso de erro na instalação do PHP.</a>
+
+    <?php
+    include("pre-rodape.php");
+    ?>
+
+    <!----------------------------------------------------------------------------------------------------------------->
+
+    <h4 ID="caso-de-erro" class="reduzido">Caso de erro na instalação do PHP.</h4>
 
     <p class="comum">J&aacute; me aconteceu uma vez que fui testar e s&oacute; dava uma tela em branco</p>
     <p class="comum">em vez de aparecerem as informa&ccedil;&odblac;es do phpinfo();</p>
